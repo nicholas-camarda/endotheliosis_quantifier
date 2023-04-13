13f20e1545ab370412806732d4a0858b7c9bed0f
Nick Camarda <62025135+nicholas-camarda@users.noreply.github.com>
2023-04-13 18:32:36 -0400

    trying to get this memory issues resovled

--- scripts/main/quantify_endotheliosis.py
+++ scripts/main/quantify_endotheliosis.py
@@ -1,56 +1,167 @@
-# print("Using VGG16 to extract features from ROIs in validation set...")
-# # Do the same thing for the validation set
-# glomerular_features_validation = new_model.predict(X_val)
-# X_val_brr, y_val_brr = make_regression_data(glomerular_features_validation, y_val)
+import os
+import pickle
+from math import sqrt
 
-# lightGBM
-# Create and train the Bayesian Ridge Regression model
-print("Running Bayesian Ridge Regression model training...")
-bayesian_ridge_model = BayesianRidge(verbose=True)
-# Set up K-Fold cross-validation
-kfold = KFold(n_splits=5, shuffle=True, random_state=1)
+import cv2
+import numpy as np
+import psutil
+from sklearn.linear_model import (ARDRegression, BayesianRidge,
+                                  LinearRegression, SGDRegressor)
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import (KFold, ShuffleSplit, cross_val_score,
+                                     train_test_split)
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
-# Calculate the cross-validated scores
-scores_cval_brr = cross_val_score(bayesian_ridge_model, X_train_brr, y_train_brr, cv=kfold, scoring='neg_mean_squared_error')
 
-# Calculate the average score and standard deviation
-average_score = np.mean(scores)
-std_score = np.std(scores)
+def load_pickled_data(file_path):
+    # Open the pickle file
+    with open(file_path, 'rb') as f:
+        # Load the data from the file
+        data = pickle.load(f)
+    return data
 
-print(f'Average score: {average_score}, Standard deviation: {std_score}')
 
-# Train the BayesianRidge model on the entire training data
-bayesian_ridge_model.fit(X_train_brr, y_train_brr)
+def openness_score(mask, preprocessed_image, threshold_ratio=0.85):
+    # Calculate the area of the glomerulus (white pixels in the mask)
+    total_area = cv2.countNonZero(mask)
+    print(f'Total area: {total_area}')
 
-# Save the trained Bayesian Ridge Regression model
-top_output_directory_regression_models = 'output/regression_models/bayesian_ridge_model'
-os.makedirs(top_output_directory_regression_models, exist_ok=True)
-model_filepath = os.path.join(top_output_directory_regression_models, 'bayesian_ridge_model-glom_openness.pkl')
+    # # Calculate the area of open capillaries (white pixels in the preprocessed image within the mask)
+    # open_area = cv2.countNonZero(cv2.bitwise_and(preprocessed_image, mask))
+    # print(f'Open area: {open_area}')
 
-with open(model_filepath, 'wb') as f:
-    pickle.dump(bayesian_ridge_model, f)
+    # Find the maximum pixel value in the preprocessed image
+    max_pixel_value = np.max(preprocessed_image)
+    print(f'Max pixel value: {max_pixel_value}')
 
-print(f"Bayesian Ridge Regression model saved to {model_filepath}")
+    # Calculate the threshold pixel value
+    threshold_pixel_value = threshold_ratio * max_pixel_value
+    print(f'Threshold pixel value: {threshold_pixel_value}')
 
-# Make predictions on the test set and calculate the prediction variance
-y_pred_brr, y_pred_var_brr = bayesian_ridge_model.predict(X_val_brr, return_std=True)
+    # Create a binary mask with maximum pixel values in the preprocessed image
+    max_pixel_mask = (preprocessed_image >=
+                      threshold_pixel_value).astype(np.uint8)
 
-# Compute the confidence intervals
-confidence_level = 0.95
-z = 1.96  # z-score for 95% confidence
-std_pred = np.sqrt(y_pred_var_brr)  # Calculate the standard deviation for each prediction
-lower_confidence_interval = y_pred_brr - z * std_pred
-upper_confidence_interval = y_pred_brr + z * std_pred
+    print(max_pixel_mask.shape)
+    print(mask.shape)
 
-# Save the predictions and confidence intervals
-predictions_filepath = os.path.join(top_output_directory_regression_models, 'validation_predictions_and_confidence_intervals.csv')
-np.savetxt(predictions_filepath, np.column_stack((y_pred_brr, lower_confidence_interval, upper_confidence_interval)), delimiter=',', header='prediction,lower_ci,upper_ci', comments='')
+    # Calculate the area of open capillaries (maximum pixel value occurrences within the mask)
+    open_area = cv2.countNonZero(cv2.bitwise_and(max_pixel_mask, mask))
+    print(f'Open area: {open_area}')
 
-print(f"Predictions and confidence intervals saved to {predictions_filepath}")
+    # Calculate the ratio of open area to total area
+    score = open_area / total_area if total_area > 0 else 0
 
-# Evaluate the model's performance
-rmse = sqrt(mean_squared_error(y_val, y_pred_brr))
-print(f"RMSE: {rmse}")
+    return score
+
+
+def grade_glomerulus(openness_score):
+    # Define the threshold values for each grade based on your ground-truth data
+    grade_thresholds = [0.6, 0.4, 0.2]  # 20% open, 40% open, 60% open
+
+    # Grade the glomerulus based on the openness score
+    for i, threshold in enumerate(grade_thresholds):
+        if openness_score >= threshold:
+            return i
+    return len(grade_thresholds)
+
+
+def print_memory_usage():
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    print(f"Memory usage: {mem_info.rss / (1024 * 1024):.2f} MB")
+
+
+top_data_directory = 'data/Lauren_PreEclampsia_Data'
+regression_model = 'bayesian_ridge_model'
+regression_cache_dir_path = os.path.join(top_data_directory, 'cache', 'regression_input', regression_model)
+top_output_directory_regresion_models = 'output/regression_models'
+directory_regression_models = os.path.join(top_output_directory_regresion_models, regression_model)
+
+n_cv_splits = 5
+n_cpu_jobs = 3  # can change this on better machine
+
+# load up the data
+X_train_brr = load_pickled_data(os.path.join(regression_cache_dir_path, 'X_train_regression.pkl'))[:20]
+y_train_brr = load_pickled_data(os.path.join(regression_cache_dir_path, 'y_train_regression.pkl'))[:20]
+X_val_brr = load_pickled_data(os.path.join(regression_cache_dir_path, 'X_val_regression.pkl'))
+y_val_brr = load_pickled_data(os.path.join(regression_cache_dir_path, 'y_val_regression.pkl'))
+
+print(f'X_train: {X_train_brr.shape}')
+print(f'y_train: {y_train_brr.shape}')
+print(f'X_val: {X_val_brr.shape}')
+print(f'y_val: {y_val_brr.shape}')
+
+# A generator function to yield batches of data
+
+
+def data_generator(X, y, batch_size):
+    n_samples = X.shape[0]
+    indices = np.arange(n_samples)
+    np.random.shuffle(indices)
+
+    for start_idx in range(0, n_samples, batch_size):
+        end_idx = min(start_idx + batch_size, n_samples)
+        batch_indices = indices[start_idx:end_idx]
+        yield X[batch_indices], y[batch_indices]
+
+
+# raise ValueError("stop")
+# Create an SGDRegressor instance
+sgd_regressor = SGDRegressor(verbose=0, penalty='elasticnet')
+
+# Define the number of epochs and batch size
+# n_epochs = 100
+# batch_size = 16
+sgd_regressor.fit(X_train_brr, y_train_brr)
+
+# # Train the SGDRegressor using a generator
+# for epoch in range(1, n_epochs + 1):
+#     print(f'Epoch {epoch}')
+#     for X_batch, y_batch in data_generator(X_train_brr, y_train_brr, batch_size):
+#         sgd_regressor.partial_fit(X_batch, y_batch)
+#     print('-' * 50)
+
+# Evaluate the model
+y_pred = sgd_regressor.predict(X_val_brr)
+mse = mean_squared_error(y_val_brr, y_pred)
+print(f'Mean squared error: {mse}')
+
+# print("Fitting ARDRegression model...")
+
+# model = ARDRegression(verbose=True, compute_score=True, copy_X=False)
+# model.fit(X_train_brr, y_train_brr)
+# print("ARDRegression model fitted successfully.")
+
+# # Save the trained Bayesian Ridge Regression model
+# os.makedirs(directory_regression_models, exist_ok=True)
+# model_filepath = os.path.join(directory_regression_models, 'ARDmodel-glom_openness.pkl')
+
+# with open(model_filepath, 'wb') as f:
+#     pickle.dump(model, f)
+
+# print(f"ARD Regression model saved to {model_filepath}")
+
+# # Make predictions on the test set and calculate the prediction variance
+# y_pred_brr, y_pred_var_brr = model.predict(X_val_brr, return_std=True)
+
+# # Compute the confidence intervals
+# confidence_level = 0.95
+# z = 1.96  # z-score for 95% confidence
+# std_pred = np.sqrt(y_pred_var_brr)  # Calculate the standard deviation for each prediction
+# lower_confidence_interval = y_pred_brr - z * std_pred
+# upper_confidence_interval = y_pred_brr + z * std_pred
+
+# # Save the predictions and confidence intervals
+# predictions_filepath = os.path.join(directory_regression_models, 'validation_predictions_and_confidence_intervals.csv')
+# np.savetxt(predictions_filepath, np.column_stack((y_pred_brr, lower_confidence_interval, upper_confidence_interval)), delimiter=',', header='prediction,lower_ci,upper_ci', comments='')
+
+# print(f"Predictions and confidence intervals saved to {predictions_filepath}")
+
+# # Evaluate the model's performance
+# rmse = sqrt(mean_squared_error(y_val_brr, y_pred_brr))
+# print(f"RMSE: {rmse}")
 
 
 if (False):
@@ -96,3 +207,32 @@ if (False):
     print(f"Mean squared error: {mse:.2f}")
     print(f"R2 score: {r2_score(y_test, y_pred):.2f}")
     print(f"Confidence interval: {confidence_interval}")
+
+
+# Create and train the Bayesian Ridge Regression model
+# print("Running Bayesian Ridge Regression model training...")
+# bayesian_ridge_model = BayesianRidge(verbose=True, compute_score=True, copy_X=False)
+
+
+# Set up K-Fold cross-validation
+# kfold = KFold(n_splits=n_cv_splits, shuffle=True, random_state=1)
+# kfold = ShuffleSplit(n_splits=n_cv_splits, test_size=0.2, random_state=42)
+
+
+# Calculate the cross-validated scores
+# print(f'Getting cross validation score with {n_cv_splits} splits...')
+# scores_cval_brr = cross_val_score(bayesian_ridge_model, X_train_brr, y_train_brr,
+#                                   scoring='neg_mean_squared_error', n_jobs=n_cpu_jobs,
+#                                   verbose=2)
+
+# Calculate the average score and standard deviation
+# average_score = np.mean(scores_cval_brr)
+# std_score = np.std(scores_cval_brr)
+
+# print(f'Average score: {average_score}, Standard deviation: {std_score}')
+
+# print_memory_usage()
+# # Train the BayesianRidge model on the entire training data
+# bayesian_ridge_model.fit(X_train_brr, y_train_brr)
+# print_memory_usage()
+# print("BayesianRidge model fitted successfully.")
--- scripts/main/run_feature_extractor_model.py
+++ scripts/main/run_feature_extractor_model.py
@@ -18,62 +18,13 @@ from keras.models import Model
 from matplotlib import pyplot as plt
 from numpy import absolute
 from scipy import stats
+from sklearn.model_selection import train_test_split
 # from skimage import io
 # from skimage.io import imread, imsave
-from sklearn.linear_model import BayesianRidge, LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import KFold, cross_val_score, train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 # scikit-optimize
 from skopt import BayesSearchCV
 from skopt.space import Integer, Real
 from tensorflow.keras.models import load_model
-
-
-def openness_score(mask, preprocessed_image, threshold_ratio=0.85):
-    # Calculate the area of the glomerulus (white pixels in the mask)
-    total_area = cv2.countNonZero(mask)
-    print(f'Total area: {total_area}')
-
-    # # Calculate the area of open capillaries (white pixels in the preprocessed image within the mask)
-    # open_area = cv2.countNonZero(cv2.bitwise_and(preprocessed_image, mask))
-    # print(f'Open area: {open_area}')
-
-    # Find the maximum pixel value in the preprocessed image
-    max_pixel_value = np.max(preprocessed_image)
-    print(f'Max pixel value: {max_pixel_value}')
-
-    # Calculate the threshold pixel value
-    threshold_pixel_value = threshold_ratio * max_pixel_value
-    print(f'Threshold pixel value: {threshold_pixel_value}')
-
-    # Create a binary mask with maximum pixel values in the preprocessed image
-    max_pixel_mask = (preprocessed_image >=
-                      threshold_pixel_value).astype(np.uint8)
-
-    print(max_pixel_mask.shape)
-    print(mask.shape)
-
-    # Calculate the area of open capillaries (maximum pixel value occurrences within the mask)
-    open_area = cv2.countNonZero(cv2.bitwise_and(max_pixel_mask, mask))
-    print(f'Open area: {open_area}')
-
-    # Calculate the ratio of open area to total area
-    score = open_area / total_area if total_area > 0 else 0
-
-    return score
-
-
-def grade_glomerulus(openness_score):
-    # Define the threshold values for each grade based on your ground-truth data
-    grade_thresholds = [0.6, 0.4, 0.2]  # 20% open, 40% open, 60% open
-
-    # Grade the glomerulus based on the openness score
-    for i, threshold in enumerate(grade_thresholds):
-        if openness_score >= threshold:
-            return i
-    return len(grade_thresholds)
 
 
 def load_pickled_data(file_path):
@@ -140,47 +91,12 @@ def make_regression_data(features, y):
     return X_, y_
 
 
-def tune_learning_rate_and_estimators(elr_X_train, elr_y_train, elr_cv, device_type):
-    # Define search space
-    search_space = {
-        'learning_rate': Real(0.0001, 0.3),
-        'n_estimators': Integer(50, 400),
-    }
-
-    # Set up the LightGBM model with the device type (CPU or GPU)
-    lgb_model = lgb.LGBMRegressor(device=device_type)
-
-    # Set up BayesSearchCV with the specified search space
-    bayes_search = BayesSearchCV(estimator=lgb_model, search_spaces=search_space, scoring='neg_mean_absolute_error', cv=elr_cv, n_jobs=4, n_iter=50)
-
-    # Perform the search to find the best learning rate and number of estimators
-    bayes_search.fit(elr_X_train, elr_y_train)
-
-    # Get the best learning rate and number of estimators
-    best_learning_rate = bayes_search.best_params_['learning_rate']
-    best_n_estimators = bayes_search.best_params_['n_estimators']
-    print(f'Best learning rate: {best_learning_rate}')
-    print(f'Best number of estimators: {best_n_estimators}')
-
-    return best_learning_rate, best_n_estimators
-
-
-physical_devices = tf.config.list_physical_devices('GPU')
-if physical_devices:
-    try:
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
-        print("Memory growth set")
-    except RuntimeError as e:
-        print(e)
-
 top_data_directory = 'data/Lauren_PreEclampsia_Data'
 cache_dir_path = os.path.join(top_data_directory, 'cache')
 top_output_directory = 'output/segmentation_models'
 top_output_directory_regresion_models = 'output/regression_models'
 file_name_with_ext = f'2023-04-10-glom_unet_xfer_seg_model-epochs75_batch8.hdf5'
 file_name = os.path.splitext(file_name_with_ext)[0]
-
-val_perc = 0.2
 
 # make the output directories
 final_output_path = os.path.join(
